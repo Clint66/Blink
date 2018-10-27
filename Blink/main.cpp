494fc331efa0a24e3dcd88717ce7339bc9b82b47
Clint66 <c.foote@unilink.com.au>
2018-10-27 14:54:52 +1100

    Blink

---
+++ Blink/main.cpp
@@ -0,0 +1,36 @@
+/*
+* Blink.cpp
+*
+* Created: 26/10/2018 6:20:45 PM
+* Author : clint
+*/
+
+# define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+
+#define PIN PB0
+#define DELAY 1000
+
+int main(void)
+{
+
+	DDRB = (1<<PIN);
+	PORTB |= (1<<PIN);
+
+	while (1)
+	{
+		_delay_ms(DELAY);	
+
+		PORTB &= ~(1<<PIN);
+
+		_delay_ms(DELAY);	
+
+		PORTB |= (1<<PIN);
+
+	}
+}
+
